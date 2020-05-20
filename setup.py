592e3698228f7d3c345c23955d54a6100867d231
Sean Breckenridge <seanbrecke@gmail.com>
2020-05-20 03:46:35 -0700

    version 0.1.3:
    * improve typing
    * make the todo.txt file optional
    if the user doesnt provide one,
    it tests ~/.config/todo/todo.txt
    and ~/.todo/todo.txt.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with io.open('README.md', encoding='utf-8') as fo:
 
 setup(
     name='full-todotxt',
-    version="0.1.2",
+    version="0.1.3",
     url='https://github.com/seanbreckenridge/full_todotxt',
     author='Sean Breckenridge',
     author_email='seanbrecke@gmail.com',
